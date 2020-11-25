e79b8cbd05e760c9afc4391a739b2e8a05d428ff
shakiogunlesi <shaki_ogunlesi@outlook.com>
2020-11-25 23:16:03 +0100

    Create Simple Array Question.cpp

---
+++ Simple Array Question.cpp
@@ -0,0 +1,44 @@
+Simple Array Question
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    /*
+     * Complete the simpleArraySum function below.
+     */
+    static int simpleArraySum(int[] ar) {
+       int Sum = 0;
+            if(ar.Count() > 1000)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < ar.Count(); i++)
+            {
+                Sum += ar[i];
+            }
+
+            return Sum;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
